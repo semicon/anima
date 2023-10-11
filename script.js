daa2d69685ae67f452f24bad3d7a30b19f300483
Wichy Ph.  (PhD.) <30399464+semicon@users.noreply.github.com>
2023-10-11 11:16:31 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+/** Dump Truck Loading Animation **/
+function loading() {
+  let percents = [0.25, 0.5, 0.75, 1];
+  let step = 0;
+  let truckLoopDur = 10;
+  let fill = function () {
+    let fillEl = document.querySelector(".progress-fill");
+    fillEl.style.transform = "scaleY(" + percents[step] + ")";
+    ++step;
+    if (step < percents.length) {
+      setTimeout(fill, (truckLoopDur * 1e3) / 2);
+    }
+  };
+  setTimeout(fill, (truckLoopDur * 1e3) / 4);
+}
